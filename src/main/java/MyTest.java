63f11611dcce7ff4fb2b877ad33091ce1b2009ab
Jay <854108766@qq.com>
2022-07-27 13:07:48 +0800

    汤世佳-克隆项目后新建分支-添加编写的代码文件

---
+++ src/main/java/MyTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author Dragon code!
+ * @create 2022-07-27 13:05
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("克隆项目后新建分支，然后测试提交代码！");
+    }
+}
